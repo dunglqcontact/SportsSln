e292a2e88b9e23bb527f219e25d876b4a386023d
dunglqcontact <dunglq.contact@gmail.com>
2021-04-17 08:17:27 +0700

    chapter9

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using SportsStore.Models;
+using System.Linq;
 using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
@@ -11,26 +8,30 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         private IStoreRepository repository;
-        public int PageSize = 4;
+        public int PageSize = 3;
 
-        public HomeController(IStoreRepository respo)
+        public HomeController(IStoreRepository repo)
         {
-            this.repository = respo;
+            repository = repo;
         }
 
-        public ViewResult Index(int productPage = 1)
-            => View(new ProductsListViewModel
-            {
-                Products = repository.Products
-                .OrderBy(p => p.ProductID)
-                .Skip((productPage - 1) * PageSize)
-                .Take(PageSize),
-                PagingInfo = new PagingInfo
-                {
-                    CurrentPage = productPage,
-                    ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
-                }
-            });
+        public ViewResult Index(string category, int productPage = 1)
+           => View(new ProductsListViewModel
+           {
+               Products = repository.Products
+                   .Where(p => category == null || p.Category == category)
+                   .OrderBy(p => p.ProductID)
+                   .Skip((productPage - 1) * PageSize)
+                   .Take(PageSize),
+               PagingInfo = new PagingInfo
+               {
+                   CurrentPage = productPage,
+                   ItemsPerPage = PageSize,
+                   TotalItems = category == null ?
+                        repository.Products.Count() :
+                        repository.Products.Where(e =>
+                            e.Category == category).Count()
+               }
+           });
     }
 }
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -15,36 +15,56 @@ namespace SportsStore
 {
     public class Startup
     {
+
         public Startup(IConfiguration config)
         {
             Configuration = config;
         }
+
         private IConfiguration Configuration { get; set; }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<StoreDbContext>(opts => {
+
+            services.AddDbContext<StoreDbContext>(opts =>
+            {
                 opts.UseSqlServer(
-                Configuration["ConnectionStrings:SportsStoreConnection"]);
+                    Configuration["ConnectionStrings:SportsStoreConnection"]);
             });
             services.AddScoped<IStoreRepository, EFStoreRepository>();
+            services.AddRazorPages();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute("pagination", "Products/Page{productPage}", new { Controller = "Home", action = "Index"});
+                //Shows the specified page (in this case, page 2) of items from the specified category (in this case, Soccer)
+                endpoints.MapControllerRoute("catpage",
+                    "{category}/Page{productPage:int}",
+                    new { Controller = "Home", action = "Index" });
+                //Lists the specified page (in this case, page 2), showing items from all categories
+                endpoints.MapControllerRoute("page", "Page{productPage:int}",
+                    new { Controller = "Home", action = "Index", productPage = 1 });
+                //Shows the first page of items from a specific category(in this case, the Soccer category)
+                endpoints.MapControllerRoute("category", "{category}",
+                    new { Controller = "Home", action = "Index", productPage = 1 });
+                endpoints.MapControllerRoute("pagination",
+                    "Products/Page{productPage}",
+                    new { Controller = "Home", action = "Index", productPage = 1 });
                 endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
             });
+
             SeedData.EnsurePopulated(app);
         }
     }
